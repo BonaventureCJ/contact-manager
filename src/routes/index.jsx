fa453767e99c401454ed8ab212d29fac117e1a88
BonaventureCJ <ugwubonaventure@gmail.com>
2024-02-24 01:09:14 +0100

    init vite project

---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+export default function Index() {
+  return (
+    <article className="idle-state">
+      <img
+        width="60"
+        height="60"
+        src="https://img.icons8.com/officel/60/contacts.png"
+        alt="contacts"
+        className="contact--logo__lg"
+      />
+
+      <h4 id="zero-state">Create or Select a Contact to View/Edit:</h4>
+    </article>
+  );
+}
